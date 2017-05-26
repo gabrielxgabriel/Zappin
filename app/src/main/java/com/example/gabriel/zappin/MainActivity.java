a4591e5fb529fa0e6e83e92d2efae408f59a7d3f
Gabriel Alves <gabrielbh304@gmail.com>
2017-05-26 18:12:54 -0300

    Adição da tela com menu lateral, e otimização do layout

--- app/src/main/java/com/example/gabriel/zappin/MainActivity.java
+++ app/src/main/java/com/example/gabriel/zappin/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent homeIntent = new Intent(MainActivity.this, MenuActivity.class);
                 startActivity(homeIntent);
                 finish();
 
